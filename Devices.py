79fabbc2a075fa0c4db7a7b5321c7b27ea3cb268
ali Boudakhane <boudakhaneali@gmail.com>
2020-12-01 12:05:28 +0100

    intial files

---
+++ Devices.py
@@ -0,0 +1,69 @@
+class Device :
+    ipAddress=""
+    port = ""
+
+    def findDevices() :
+            devices=[]
+
+            d= Device()
+            d.ipAddress= "192.168.1.1"
+            d.port="8080"
+
+            devices.append(d)
+            
+            d= Device()
+            d.ipAddress= "192.168.1.2"
+            d.port="443"
+            
+            devices.append(d)
+
+            d= Device()
+            d.ipAddress= "192.168.1.3"
+            d.port="23"
+            
+            devices.append(d)
+
+            return devices
+
+
+
+
+
+class DevicesView :
+    def showDevices(self , devices):
+        for d in devices :
+            print( "------------------------" ) 
+            print ("Ip address : " + str(d.ipAddress) )
+            print ("Port : " + str(d.port))
+            print( "------------------------" ) 
+
+
+
+
+
+
+
+class DevicesController : 
+    def __init__(self) :
+        devices = Device.findDevices()
+
+        v= DevicesView()
+        v.showDevices(devices)
+
+
+
+
+
+c =DevicesController()
+
+
+ 
+
+            
+
+
+
+
+
+
+        
---
+++ test.py
@@ -0,0 +1,14 @@
+x=0
+while  x <=10 :   
+    print("this is cool")
+    x=x+1
+while  x>10 :
+    print ("Go On...");
+    print (x)
+    x=x+1
+    if x ==100 or x==101 :
+        print("stop")
+        break
+
+
+
